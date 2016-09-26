4faa2ad9398d737b312cb732404151118f0a0da1
KwakesProject <kwakes@me.com>
2016-09-26 16:46:22 +0100

    Components - Stateless Example

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Content/>
+      </div>
+    );
+  }
+}
 
-     var myStyle = {
-         fontSize: 100,
-         color: '#FF0000'
-      }
-      return (
-         <div>
-           <h1 style = {myStyle}>Header</h1>
-           <h2>Content</h2>
-           <p data-myattribute = "somevalue">This is the content!!!</p>
-           <h1>{1+1}</h1>
-         </div>
-      );
-   }
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    );
+  }
+}
+
+class Content extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Content</h2>
+        <p>The content text!!!</p>
+      </div>
+    );
+  }
 }
 
 export default App;
