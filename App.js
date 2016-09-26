d70bc25406ce533823dd9a878c71846a942b9563
KwakesProject <kwakes@me.com>
2016-09-26 16:43:30 +0100

    jsx examples

--- App.js
+++ App.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 class App extends React.Component {
    render() {
+
+     var myStyle = {
+         fontSize: 100,
+         color: '#FF0000'
+      }
       return (
          <div>
-            Hello World!!!
+           <h1 style = {myStyle}>Header</h1>
+           <h2>Content</h2>
+           <p data-myattribute = "somevalue">This is the content!!!</p>
+           <h1>{1+1}</h1>
          </div>
       );
    }
