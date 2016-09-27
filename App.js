6762791db1e5b37cd0685cc0480207cb9e6cafd4
KwakesProject <kwakes@me.com>
2016-09-27 10:26:21 +0100

    React Router part done but set up in a different way (components seperated)

--- App.js
+++ App.js
@@ -1,52 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router'
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      data:
-      [
-        {
-          component: 'First...',
-          id: 1
-        },
-
-        {
-          component: 'Second...',
-          id: 2
-        },
-
-        {
-          component: 'Third...',
-          id: 3
-        }
-      ]
-    }
-
-  }
-
   render() {
     return (
       <div>
-        <div>
-          {this.state.data.map((dynamicComponent, i) => <Content
-            key = {i} componentData = {dynamicComponent}/>) }
-        </div>
+        <ul>
+          <li><Link to='home'>Home</Link></li>
+          <li><Link to='about'>About</Link></li>
+          <li><Link to='contact'>Contact</Link></li>
+        </ul>
+
+        {this.props.children}
       </div>
-    );
+    )
   }
 }
 
-class Content extends React.Component {
-  render() {
-    return (
-      <div>
-        <div>{this.props.componentData.component}</div>
-        <div>{this.props.componentData.id}</div>
-      </div>
-    );
-  }
-}
 
 export default App;
